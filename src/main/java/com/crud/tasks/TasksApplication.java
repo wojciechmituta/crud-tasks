acb81e8a8921c5e5f6ab3fe1f4e3674b36df4a06
wojciechmituta <38225947+wojciechmituta@users.noreply.github.com>
2018-06-15 20:45:10 +0200

    Tasks v.1.0.0

---
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -0,0 +1,14 @@
+package com.crud.tasks;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApplication {
+
+    public static void main(String[] args) {
+        TaskDto taskDto = new TaskDto((long) 2, "dadasd", "dsaad");
+        SpringApplication.run(TasksApplication.class, args);
+    }
+}
