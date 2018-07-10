8f8a849d85a7e8b52b2591a739beab4466f636a1
wojciechmituta <38225947+wojciechmituta@users.noreply.github.com>
2018-07-10 22:52:56 +0200

    swagger-only tasks.controller

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -25,7 +25,7 @@ public class CoreConfiguration extends WebMvcConfigurerAdapter {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
