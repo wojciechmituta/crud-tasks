ff2481e11c0f288fe066384e7010aaeea1340058
wojciechmituta <38225947+wojciechmituta@users.noreply.github.com>
2018-07-12 19:59:18 +0200

    add Email Service

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,44 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+
+    public void shouldSendEmail() {
+
+        //Given
+        Mail mail = new Mail("test@test.com", "test1@test.com", "Test", "Test Message");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+
+}
